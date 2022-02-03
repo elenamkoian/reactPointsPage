36a993160c7d40c467ba2bb461b12f4b1442bf21
elena <elenamkoian@mail.com>
2022-02-03 13:52:59 +0400

    [add] box shadow to navbar
     # other changes
     * [remove] slice folder from store

--- src/componetnts/navbar/navbar.js
+++ src/componetnts/navbar/navbar.js
@@ -9,6 +9,7 @@ const useStyles = makeStyles((theme) => ({
       height: theme.spacing(9),
       display: 'flex',
       justifyContent: 'space-between',
+      boxShadow: '5px 5px 10px rgba(0, 0, 0, 0.5)',
     },
     ButtonList: {
       display: 'flex',
